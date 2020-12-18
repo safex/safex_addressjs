699a5fe4eeb2101a52578de7bf6c5b9f4c9e71cd
Ivan Pantic <ivan@pantas.net>
2020-12-18 23:31:03 +0100

    Allow usage of library only after calling initialize()

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const cnUtil = require('./lib/cnUtil');
-const { cn_fast_hash_safex, check_signature, sign_message, sc_reduce32, rand_32, create_address, pubkeys_to_string, address_checksum, sec_key_to_pub, structure_keys, decode_address, verify_checksum } = cnUtil;
+const {
+    cn_fast_hash_safex,
+    check_signature,
+    sign_message,
+    sc_reduce32,
+    rand_32,
+    create_address,
+    pubkeys_to_string,
+    address_checksum,
+    sec_key_to_pub,
+    structure_keys,
+    decode_address,
+    verify_checksum,
+    initialize
+} = cnUtil;
 
 module.exports = {
     sc_reduce32,
@@ -13,10 +27,11 @@ module.exports = {
     verify_checksum,
     sign_message,
     check_signature,
-    cn_fast_hash_safex
+    cn_fast_hash_safex,
+    initialize
 };
 
 // Bind the exports to cnUtil
 for (const key in module.exports) {
-  module.exports[key] = module.exports[key].bind(cnUtil);
+    module.exports[key] = module.exports[key].bind(cnUtil);
 }
